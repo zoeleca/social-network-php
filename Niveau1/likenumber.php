7f2dc1a8c6131374ff8470abd31d2b41eaac1e78
zoeleca <zoennts@gmail.com>
2024-02-13 10:29:33 +0100

    like number

---
+++ Niveau1/likenumber.php
@@ -0,0 +1,28 @@
+<?php
+// Step 1 - check if the form was submitted 
+if (isset($post['like_number'])) {
+    $userId = $_SESSION['connected_id'];
+    $postId = intval($post['id']);
+
+    //$request = "SELECT * FROM followers ";
+    //$SQL = $mysqli->query($request);
+
+
+    $postId = intval($mysqli->real_escape_string($postId));
+    $userId = $mysqli->real_escape_string($userId);
+
+
+    $SQL ="INSERT INTO likes"
+    . "(id, user_id, post_id) "
+    . "VALUES (NULL, $userId, $postId );";
+
+    $ok = $mysqli->query($SQL);
+    if (!$ok) {
+        echo "Impossible de like " . $mysqli->error;
+    } else {
+        echo "liked"  ;
+    }
+
+}
+
+?>
